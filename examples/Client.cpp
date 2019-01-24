3085454f7fa01ef7bdb9d3e9c77af43456612fef
beamanbecool <461598436@qq.com>
2019-01-24 23:10:19 +0800

    modify client class for net samples

--- examples/Client.cpp
+++ examples/Client.cpp
@@ -8,70 +8,156 @@
 #include "Message.h"
 #include <errno.h>
 #include <logging.h>
-Client::Client(int clientFd)
-        : fd_(clientFd)
-        , recvLen_(0)
-        , needRecvLen_(HEADER_LEN)
-        , sendLen_(0)
-        , needSendLen_(0)
-        , stat_(RECVHEAD)
+#include <string.h>
+
+Client::Client(int clientFd, const string &id)
+    : fd_(clientFd),
+      id_(id),
+      recvLen_(0),
+      needRecvLen_(HEADER_LEN),
+      sendLen_(0),
+      needSendLen_(0),
+      recvStat_(RECVED),
+      sendStat_(SENDED),
+      recvBuf_(DEFAULT_BUFF_LEN)
 {
 
 }
 
-bool Client::recvOnePck() {
-    char* curPos = &recvBuf_[recvLen_];
-    int recvLen = read(fd_, curPos, needRecvLen_);
-    if(recvLen == 0){setStat_(TOCLOSE); return false;}//对端关闭连接
-    if(recvLen == -1){
-        if(errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN){
-            return false; //需要继续接收数据
-        } else{
-            LOG(ERROR) << "unexpected error! need close this client, errno:" << errno;
-            setStat_(TOCLOSE);
-        }
-    } else{
-        recvLen_ += recvLen;
-        if(recvLen_ < needRecvLen_){    //还没收全一帧数据
-            return false;
-        } else{                        //收全头或者完整帧
-            if(getStat_() == RECVHEAD){
-                needRecvLen_ = int((int*)&recvBuf_[0]); //收完头继续收payload
-                return false;
-            } else{                         //收完完整一帧
-                return true;
-            }
-        }
+bool Client::recvOnePck()
+{
+  if (getRecvStat_() == RECVED)
+    return false;
+
+  int recvLen = read(fd_, &recvBuf_[recvLen_], needRecvLen_);
+  if (recvLen == 0)//对端关闭连接
+  {
+    setRecvStat_(TOCLOSE);
+    LOG(ERROR) << "client:" << id_ << " closed!";
+    return false;
+  }
+  if (recvLen == -1)
+  {
+    if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
+    {
+      return false; //需要继续接收数据
     }
+    else
+    {
+      LOG(ERROR) << "unexpected recv error! need close this client:" << id_ << ", errno:" << errno;
+      setRecvStat_(TOCLOSE);
+      return false;
+    }
+  }
+  if (recvLen < needRecvLen_)//还没收全一帧数据
+  {
+    recvLen_ += recvLen;
+    needRecvLen_ -= recvLen;
+    return false;
+  }
+  else
+  {
+    if (getRecvStat_() == RECVHEAD)  //收完头继续收payload
+    {
+      needRecvLen_ = *（(int *) &recvBuf_[0]);
+      recvBuf_.resize(HEADER_LEN + needRecvLen_);
+      setRecvStat_(RECVBODY);
+      return false;
+    }
+    else if (getRecvStat_() == RECVBODY)
+    {
+      setRecvStat_(RECVED);
+      return true;
+    }
+  }
 }
 
-bool Client::getPck(vector<char>& recvPck) {
+bool Client::getPck(vector<char> &recvPck)
+{
+  if (getRecvStat_() == RECVED)
+  {
     recvPck = recvBuf_;
+    setRecvStat_(RECVHEAD);//取走数据继续接收
+    recvLen_ = 0;
     return true;
+  }
+  return false;
 }
 
-bool Client::sendPck() {
-    return false;
+bool Client::sendPck()
+{
+  if (getSendStat_() == SENDING)
+  {
+    int sendLen = write(fd_, &sendBuf_[sendLen_], sendBuf_.size() - sendLen_);
+    if (sendLen == -1)
+    {
+      if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
+      {
+        return false; //需要继续发送数据
+      }
+      else
+      {
+        setSendStat_(TOCLOSE);
+        LOG(ERROR) << "unexpected send error! need close this client:" << id_ << ", errno:" << errno;
+      }
+    }
+    if (sendLen == 0)
+    {
+      setSendStat_(TOCLOSE);
+      LOG(ERROR) << "client:" << id_ << " closed!";
+      return false;
+    }
+
+    sendLen_ += sendLen;
+    if (sendLen_ == sendBuf_.size())
+    {
+      setSendStat_(SENDED);
+      return true;
+    }
+  }
+  return false;
 }
 
-bool Client::setPck(const string &sendPck) {
-    return false;
+bool Client::setPck(const vector<char> &sendPck)
+{
+  if (getSendStat_() == SENDED)
+  {
+    int dataLen = sendPck.size();
+    sendBuf_.resize(HEADER_LEN + dataLen);
+    memcpy(&sendBuf_[0], &dataLen, HEADER_LEN);
+    memcpy(&sendBuf_[0] + HEADER_LEN, &sendPck[0], dataLen);
+    setSendStat_(SENDING);
+    return true;
+  }
+  return false;
 }
 
-int Client::getFd_() const {
-    return fd_;
+int Client::getFd_() const
+{
+  return fd_;
 }
 
-void Client::setFd_(int fd_) {
-    Client::fd_ = fd_;
+void Client::setFd_(int fd_)
+{
+  Client::fd_ = fd_;
 }
 
-int Client::getStat_() const {
-    return stat_;
+int Client::getRecvStat_() const
+{
+  return recvStat_;
 }
 
-void Client::setStat_(int stat_) {
-    Client::stat_ = stat_;
+void Client::setRecvStat_(int stat_)
+{
+  Client::recvStat_ = stat_;
+}
+int Client::getSendStat_() const
+{
+  return sendStat_;
+}
+void Client::setSendStat_(int sendStat_)
+{
+  Client::sendStat_ = sendStat_;
 }
 
 
--- examples/Client.h
+++ examples/Client.h
@@ -6,42 +6,46 @@
 #define SEVICEFRAMEWORK_CLIENT_H
 
 #include <vector>
+#include <string>
 #include "Message.h"
 
 using namespace std;
 
-class Client {
+class Client
+{
 #define DEFAULT_BUFF_LEN (1024) //1k
 #define RECV_BUFF_MAX_SIZE (1024 * 1024 * 500) //500M
 #define SEND_BUFF_MAX_SIZE (1024 * 1024 * 100) //100M 均需根据实际业务作调整，比如即时通信客户端多，buffer小，取较小合适值即可
-    enum {STARED, RECVING, RECVED, SENDING, SENDED, TOCLOSE, RECVHEAD, RECVBODY};
-public:
-    Client(int clientFd);
+  enum
+  {
+    RECVING, RECVHEAD, RECVBODY, RECVED, SENDING, SENDED, TOCLOSE
+  };
+ public:
+  Client(int clientFd, const string &id);
+  ~Client();
+  bool recvOnePck();
+  bool getPck(vector<char> &recvPck);
+  bool sendPck();
+  bool setPck(const vector<char> &sendPck);
+  int getFd_() const;
+  void setFd_(int fd_);
+  int getRecvStat_() const;
+  void setRecvStat_(int stat_);
+  int getSendStat_() const;
+  void setSendStat_(int sendStat_);
 
-    ~Client();
-    bool recvOnePck();
-    bool getPck(vector<char>& recvPck);
-    bool sendPck();
-    bool setPck(const vector<char>& sendPck);
-
-    int getFd_() const;
-
-    void setFd_(int fd_);
-
-    int getStat_() const;
-
-    void setStat_(int stat_);
-
-private:
-    int fd_;
-    int recvLen_;
-    int needRecvLen_;
-    int sendLen_;
-    int needSendLen_;
-    int stat_;
-    vector<char> recvBuf_(DEFAULT_BUFF_LEN);
-    vector<char> sendBuf_(DEFAULT_BUFF_LEN);
+ private:
+  int fd_;
+  string id_;
+  int recvLen_;
+  int needRecvLen_;
+  int sendLen_;
+  int needSendLen_;
+  int recvStat_;
+  int sendStat_;
+  vector<char> recvBuf_;
+  vector<char> sendBuf_;
 };
 
-
 #endif //SEVICEFRAMEWORK_CLIENT_H
+
