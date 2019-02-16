52b8846480c76fa7f847dfd1d05c731cdb5797a3
beamanbecool <461598436@qq.com>
2019-02-17 00:13:13 +0800

    add new connection message

---
+++ examples/NewConnectMessage.h
@@ -0,0 +1,19 @@
+//
+// Created by Shinan on 2019/2/16.
+//
+
+#ifndef SERVICEFRAMEWORK_NEWCONNECTMESSAGE_H
+#define SERVICEFRAMEWORK_NEWCONNECTMESSAGE_H
+
+#include "Message.h"
+
+class NewConnectMessage : public Message
+{
+public:
+  NewConnectMessage(int clientFd) :Message("NEW_CONNECT"),clientFd_(clientFd){}
+  int getClientFd(){return clientFd_}
+private:
+  int clientFd_;
+};
+
+#endif //SERVICEFRAMEWORK_NEWCONNECTMESSAGE_H
