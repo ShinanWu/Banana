9ac2540f3467805bf6e3a0fd9765206d6c0a5e4c
beamanbecool <461598436@qq.com>
2019-02-17 15:16:37 +0800

    complete io buffer design, made transferring data so easy

--- network/Stream.cpp
+++ network/Stream.cpp
@@ -1,167 +1,198 @@
 //
 // Created by Shinan on 2019/1/23.
 //
-//每一个连接的客户端生成一个client对象,封装了接收和发送消息
-//非线程安全
+//Stream类封装了socket的io buffer，目前简单实现，每次最大1K数据，后面仍需考虑大数据小数据的情况
+//非线程安全，不要尝试在其他线程操作此类实例
 
 #include <unistd.h>
-#include "Stream.h"
-#include "Message.h"
 #include <errno.h>
 #include <logging.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <assert.h>
+#include "Stream.h"
+#include "Message.h"
 
-Stream::Stream(int fd)
+using namespace std::placeholders;
+
+Stream::Stream(int fd, const SpEventReactor &spEventReactor)
     : fd_(fd),
-      recvLen_(0),
-      needRecvLen_(HEADER_LEN),
-      sendLen_(0),
-      needSendLen_(0),
-      recvStat_(RECVED),
-      sendStat_(SENDED),
-      recvBuf_(DEFAULT_BUFF_LEN)
+      spEventReactor_(spEventReactor)
 {
-
+  //构造函数谨慎使用this
+  recvOnePackCall_ = std::bind(&Stream::_recvOnePack, this, _1, _2);
+  sendOnePackCall_ = std::bind(&Stream::_sendOnePack, this, _1, _2);
 }
 
-StreamStat Stream::recvOnePck()
+Stream::~Stream()
 {
-  if (getRecvStat_() == RECVED)
-    return RECVED;
-
-  int recvLen = read(fd_, &recvBuf_[recvLen_], needRecvLen_);
-  if (recvLen == 0)//对端关闭连接
+  if (fd_ > 0)
   {
-    setRecvStat_(TOCLOSE);
-    LOG(ERROR) << "client:" << fd_ << " closed!";
-    return TOCLOSE;
+    close(fd_);
+    fd_ = -1;
   }
-  if (recvLen == -1)
+}
+
+void Stream::_recvOnePack(int fd, short event)
+{
+  assert(fd_ == fd && event & EventReactor::EVENT_READ);
+  int recvLen = recv(fd_, &recvBuf_[recvLen_], needRecvLen_, 0);
+  //对端关闭连接
+  if (recvLen == 0)
   {
+    if (closeCallback_)
+    { closeCallback_(fd_); }
+    recvStat_ = RECVERR;
+    LOG(ERROR) << "client:" << fd_ << " closed!";
+    recvCompleteCallback_(recvStat_, recvBuf_);
+    return;
+  }
+  if (recvLen < 0)
+  {
+    //此类错误需要继续接收数据
     if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
     {
-      return getRecvStat_(); //需要继续接收数据
+      spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_READ, recvOnePackCall_);
+      recvStat_ = RECVING;
+      return;
     }
     else
     {
-      LOG(ERROR) << "unexpected recv error! need close this client:" << fd_ << ", errno:" << errno;
-      setRecvStat_(TOCLOSE);
-      return TOCLOSE;
+      LOG(ERROR) << "unexpected recv error! shout down recv," << " errno:" << errno;
+      spEventReactor_->removeEventHandler(fd_, EventReactor::EVENT_READ);
+      if (closeCallback_)
+      { closeCallback_(fd_); }
+      //close fd，析构的时候再close,和stream生命周期保持一致
+      recvStat_ = RECVERR;
+      recvCompleteCallback_(recvStat_, recvBuf_);
+      return;
     }
   }
-  if (recvLen < needRecvLen_)//还没收全一帧数据
+  //还没收全数据
+  if (recvLen < needRecvLen_)
   {
     recvLen_ += recvLen;
     needRecvLen_ -= recvLen;
-    return getRecvStat_();
+    //需要继续接收数据
+    spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_READ, recvOnePackCall_);
+    recvStat_ = RECVING;
+    return;
   }
+    //收到全部数据
   else
   {
-    if (getRecvStat_() == RECVHEAD)  //收完头继续收payload
-    {
-     // needRecvLen_ = *（(int *)((char*)(&recvBuf_[0]));
-      recvBuf_.resize(HEADER_LEN + needRecvLen_);
-      setRecvStat_(RECVBODY);
-      return getRecvStat_();
-    }
-    else if (getRecvStat_() == RECVBODY)
-    {
-      setRecvStat_(RECVED);
-      return getRecvStat_();
-    }
+    recvStat_ = RECVED;
+    assert(recvCompleteCallback_);
+    recvCompleteCallback_(recvStat_, recvBuf_);
   }
 }
 
-bool Stream::getPck(string &recvPck)
+void Stream::_sendOnePack(int fd, short event)
 {
-  if (getRecvStat_() == RECVED)
-  {
-    recvPck = string(&recvBuf_[0], recvBuf_.size());//移动构造提高效率
-    setRecvStat_(RECVHEAD);//取走数据继续接收
-    recvLen_ = 0;
-    return true;
-  }
-  return false;
-}
+  assert(fd_ == fd && event & EventReactor::EVENT_WRITE);
 
-StreamStat Stream::sendPck()
-{
-  if(getSendStat_() == SENDED)
+  int sendLen = send(fd_, &sendBuf_[sendLen_], needSendLen_, 0);
+  if (sendLen < 0)
   {
-    return getSendStat_();
+    //继续发送
+    if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
+    {
+      sendStat_ = SENDING;
+      spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_WRITE, sendOnePackCall_);
+      return;
+    }
+    else
+    {
+      LOG(ERROR) << "unexpected send error! shout down send:" << fd_ << ", errno:" << errno;
+      sendStat_ = SENDERR;
+      sendCompleteCallback_(sendStat_);
+      return;
+    }
   }
-  if (getSendStat_() == SENDING)
+  if (sendLen == 0)
   {
-    int sendLen = write(fd_, &sendBuf_[sendLen_], sendBuf_.size() - sendLen_);
-    if (sendLen == -1)
-    {
-      if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
-      {
-        return getSendStat_(); //需要继续发送数据
-      }
-      else
-      {
-        setSendStat_(TOCLOSE);
-        LOG(ERROR) << "unexpected send error! need close this client:" << fd_ << ", errno:" << errno;
-        return getSendStat_();
-      }
-    }
-    if (sendLen == 0)
-    {
-      setSendStat_(TOCLOSE);
-      LOG(ERROR) << "client:" << fd_ << " closed!";
-      return getSendStat_();
-    }
-
+    LOG(ERROR) << "client:" << fd_ << " closed!";
+    sendStat_ = SENDERR;
+    sendCompleteCallback_(sendStat_);
+    return;
+  }
+  //继续发送
+  if (sendLen < needSendLen_)
+  {
     sendLen_ += sendLen;
-    if (sendLen_ == sendBuf_.size())
-    {
-      setSendStat_(SENDED);
-      return getSendStat_();
-    }
+    needSendLen_ -= sendLen;
+    sendStat_ = SENDING;
+    spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_WRITE, sendOnePackCall_);
+    return;
+  }
+  //发送完毕
+  else
+  {
+    sendStat_ = SENDED;
+    sendCompleteCallback_(sendStat_);
   }
 }
 
-bool Stream::setPck(const string &sendPck)
-{
-  if (getSendStat_() == SENDED)
-  {
-    int dataLen = sendPck.size();
-    sendBuf_.resize(HEADER_LEN + dataLen);
-    memcpy(&sendBuf_[0], &dataLen, HEADER_LEN);
-    memcpy((char*)(&sendBuf_[0]) + HEADER_LEN, sendPck.data(), dataLen);
-    setSendStat_(SENDING);
-    return true;
-  }
-  return false;
-}
+//bool Stream::setPck(const string &sendPck)
+//{
+//  if (getSendStat_() == SENDED)
+//  {
+//    int dataLen = sendPck.size();
+//    sendBuf_.resize(HEADER_LEN + dataLen);
+//    memcpy(&sendBuf_[0], &dataLen, HEADER_LEN);
+//    memcpy((char *) (&sendBuf_[0]) + HEADER_LEN, sendPck.data(), dataLen);
+//    return true;
+//  }
+//  return false;
+//}
 
 int Stream::getFd_() const
 {
   return fd_;
 }
 
-void Stream::setFd_(int fd_)
-{
-  Stream::fd_ = fd_;
-}
-
-StreamStat Stream::getRecvStat_() const
+int Stream::getRecvStat_() const
 {
   return recvStat_;
 }
 
-void Stream::setRecvStat_(StreamStat stat_)
-{
-  Stream::recvStat_ = stat_;
-}
-StreamStat Stream::getSendStat_() const
+int Stream::getSendStat_() const
 {
   return sendStat_;
 }
-void Stream::setSendStat_(StreamStat sendStat_)
+
+void Stream::setOnCloseCallback_(SocketCloseCallback &closeCallback)
 {
-  Stream::sendStat_ = sendStat_;
+  closeCallback_ = std::move(closeCallback); //一般只设一次
+}
+
+void Stream::asyncRecvBytes(int num, RecvCompleteCallback &recvCompleteCallback)
+{
+  if (!recvCompleteCallback || num > DEFAULT_BUFF_LEN || num <= 0)
+  {
+    LOG(ERROR) << "parameter error!";
+    return;
+  }
+  recvLen_ = 0;
+  needRecvLen_ = num;
+  recvCompleteCallback_ = recvCompleteCallback;
+  spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_READ, recvOnePackCall_);
+}
+
+void Stream::asyncSendBytes(const vector<char> &vecBytes, Stream::SendCompleteCallback &sendCompleteCallback)
+{
+  int sendSize = vecBytes.size();
+  if (!sendCompleteCallback || sendSize > DEFAULT_BUFF_LEN)
+  {
+    LOG(ERROR) << "parameter error!";
+    return;
+  }
+  sendLen_ = 0;
+  needSendLen_ = sendSize;
+  sendBuf_ = vecBytes;
+  sendCompleteCallback_ = sendCompleteCallback;
+  spEventReactor_->addEventHandler(fd_, EventReactor::EVENT_WRITE, sendOnePackCall_);
 }
 
 
--- network/Stream.h
+++ network/Stream.h
@@ -8,12 +8,10 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <functional>
 #include "Message.h"
+#include "EventReactor.h"
 
-typedef enum
-{
-  RECVING, RECVHEAD, RECVBODY, RECVED, SENDING, SENDED, TOCLOSE //流状态
-}StreamStat;
 using namespace std;
 
 class Stream
@@ -22,30 +20,47 @@ class Stream
 #define DEFAULT_BUFF_LEN (1024) //1k
 #define RECV_BUFF_MAX_SIZE (1024 * 1024 * 500) //500M
 #define SEND_BUFF_MAX_SIZE (1024 * 1024 * 100) //100M 均需根据实际业务作调整，比如即时通信客户端多，buffer小，取较小合适值即可
- public:
-  Stream(int fd);
-  ~Stream();
-  StreamStat recvOnePck();
+  typedef function<void(int fd)> SocketCloseCallback;
+  typedef function<void(int retRecvStat, const vector<char> &vecBytes)> RecvCompleteCallback;
+  typedef function<void(int retSendStat)> SendCompleteCallback;
+  enum _StreamStat
+  {
+    RECVING, RECVED, RECVERR, SENDING, SENDED, SENDERR, CLOSED //流状态
+  };
+
+public:
+  Stream(int fd, const SpEventReactor &spEventReactor);
+  virtual ~Stream();
+  void asyncRecvBytes(int num, RecvCompleteCallback &recvCompleteCallback);
+  void asyncSendBytes(const vector<char> &vecBytes, SendCompleteCallback &sendCompleteCallback);
+  void setOnCloseCallback_(SocketCloseCallback &onCloseCallback_);
+
   bool getPck(string &recvPck);
   bool setPck(const string &sendPck);
-  StreamStat sendPck();
   int getFd_() const;
-  void setFd_(int fd_);
-  StreamStat getRecvStat_() const;
-  void setRecvStat_(StreamStat stat_);
-  StreamStat getSendStat_() const;
-  void setSendStat_(StreamStat sendStat_);
+  int getRecvStat_() const;
+  int getSendStat_() const;
 
- private:
+private:
+  void _recvOnePack(int fd, short event);
+  void _sendOnePack(int fd, short event);
+
+private:
   int fd_;
-  int recvLen_;
-  int needRecvLen_;
-  int sendLen_;
-  int needSendLen_;
-  StreamStat recvStat_;
-  StreamStat sendStat_;
+  int recvLen_ = 0;
+  int needRecvLen_ = 0;
+  int sendLen_ = 0;
+  int needSendLen_ = 0;
+  int recvStat_ = RECVED;
+  int sendStat_ = SENDED;
   vector<char> recvBuf_; //接收信息的buffer
   vector<char> sendBuf_; //发送的buffer
+  function<void(int fd, short event)> recvOnePackCall_;
+  function<void(int fd, short event)> sendOnePackCall_;
+  SocketCloseCallback closeCallback_;
+  RecvCompleteCallback recvCompleteCallback_;
+  SendCompleteCallback sendCompleteCallback_;
+  SpEventReactor spEventReactor_;
 };
 
 #endif //SEVICEFRAMEWORK_CLIENT_H
