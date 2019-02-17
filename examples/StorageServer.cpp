5277e637df8a97e0b34ca2ef51586eef3f9f4f71
beamanbecool <461598436@qq.com>
2019-02-17 23:48:05 +0800

    simplify example

--- examples/StorageServer.cpp
+++ examples/StorageServer.cpp
@@ -12,29 +12,33 @@
 #include <assert.h>
 #include "ThreadPool.h"
 #include "StorageService.h"
+#include <network/LibeventReactor.h>
 #include "network/NetAcceptService.h"
 #include "network/NetWorkService.h"
 
 #define REACTOR_NUM 4 //根据CPU数目来，一般可设为CPU数目相当
-#define THREAD_POOL_NUM 50 //根据测试进行调整，值不要太大
-#define LISTEN_PORT 12306
+#define THREAD_POOL_NUM 16 //根据测试进行调整，值不要太大
+#define LISTEN_PORT 10086
 
 using namespace std;
 
 class StorageServer : public TcpServer
 {
 public:
-  StorageServer(int eventReactorNum, int threadPoolNum, unsigned short listenPort)
-      :TcpServer(eventReactorNum, listenPort)
-  {}
-  void start()
+  StorageServer(int eventReactorNum, unsigned short listenPort, int threadPoolNum)
+      :TcpServer(eventReactorNum, listenPort, threadPoolNum) {}
+
+private:
+  virtual void onConnection(const SpStream &spStream)
   {
-    LOG(INFO) << "StorageServer started!";
-    TcpServer::start();
+    LOG(INFO) << "new connection!";
   }
 
-};
+  void onRecvOnePack(int retStat, const vector<char> &vecBytes)
+  {
 
+  }
+};
 
 int initLogging()
 {
@@ -45,23 +49,14 @@ int initLogging()
   FLAGS_max_log_size = 4; //每个日志文件4M
 }
 
-
 int main()
 {
   signal(SIGPIPE, SIG_IGN);//忽略pipe破裂信号
   initLogging();
-  //启动线程池
-  ThreadPool::initInstance(50)->start();
-  shared_ptr<EventReactor> spEventReactor(new LibeventRector);
-  shared_ptr<NetAcceptService> spNetAcceptService(new NetAcceptService("NetListen", spEventReactor));
-  for(int count = 0; count < 50; count++)
-  {
-    stringstream ss;
-    ss << "Service-" << count;
-    shared_ptr<Task> spTask(new StorageService(ss.str()));
-    ThreadPool::getInstance()->syncPostTask(spTask);
-  }
-  spNetAcceptService->start(); //主线程accept
+  StorageServer storageServer(REACTOR_NUM, LISTEN_PORT, THREAD_POOL_NUM);
+  storageServer.start();
+  LOG(ERROR) << "StorageServer exit!";
+
   google::ShutdownGoogleLogging();
   return 0;
 }
