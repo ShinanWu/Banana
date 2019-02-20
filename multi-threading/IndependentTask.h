c9bb067d0518b5f14f577d4301600c0e2730ffa2
beamanbecool <461598436@qq.com>
2019-02-21 01:11:09 +0800

    fix memory corruption, don't construct string with nullptr

--- multi-threading/IndependentTask.h
+++ multi-threading/IndependentTask.h
@@ -4,7 +4,7 @@
 class IndependentTask : public Task
 {
 public:
-  IndependentTask(const string &name = nullptr);
+  IndependentTask(const string &name);
   virtual ~IndependentTask();
   virtual void start() final;
   virtual void run() = 0;//子类只需要实现run函数
